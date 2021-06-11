19c5a7eb9d35f830ddb2f3ad5118a8ccbf376013
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-10 21:09:39 -0700

    Add files via upload

---
+++ exercise2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class GFG
+{
+
+    static int mostFrequent(int[] arr, int n)
+    {
+
+      
+        Array.Sort(arr);
+
+
+        int max_count = 1, res = arr[0];
+        int curr_count = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] == arr[i - 1])
+                curr_count++;
+            else
+            {
+                if (curr_count > max_count)
+                {
+                    max_count = curr_count;
+                    res = arr[i - 1];
+                }
+                curr_count = 1;
+            }
+        }
+
+      
+        if (curr_count > max_count)
+        {
+            max_count = curr_count;
+            res = arr[n - 1];
+        }
+
+        return res;
+    }
+
+
+    public static void Main()
+    {
+
+        Console.WriteLine("please input arr length");
+        int n= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("input array");
+        String arr1 = Console.ReadLine();
+        String[] arr2 = arr1.Split(" ");
+        int[] arr = new int[arr2.Length];
+        for(int i = 0; i < arr2.Length; i++)
+        {
+            arr[i]=Convert.ToInt32(arr2[i]);
+        }
+
+
+
+
+
+
+        Console.WriteLine(mostFrequent(arr, n));
+
+    }
+}
