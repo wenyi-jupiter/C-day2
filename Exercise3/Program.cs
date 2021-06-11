2bbb8f7659d5054bbfa08775696283b2867d0469
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-10 21:36:04 -0700

    Add files via upload

---
+++ Exercise3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+
+        public int solution(int A,int B)
+
+        {
+            int count = 0;
+            for(int i = A; i <= B; i++)
+            {
+                if (Math.Sqrt(i) % 2 == 0 || Math.Sqrt(i) % 2==1)
+                {
+                    count = count + 1;
+                    
+                }
+            }
+            return count;
+            
+        }
+
+        public int A()
+        {
+            int a = 1;
+            a = a + 1;
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input A");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input B");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Program p = new Program();
+            //p.solution(A, B);
+            
+            Console.WriteLine(p.solution(A, B));
+            
+
+        }
+    }
+}
