0435d5d9ae355d9076bab19bc9bcabf374eac92a
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-10 20:08:04 -0700

    Add files via upload

---
+++ Exercise5.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    class Box
+    {
+        double length, width, height;
+        public double getVolume()
+        {
+            return length * width* height;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public void setWidth(double bre)
+        {
+            width = bre;
+        }
+
+        public void setHeight(double hei)
+        {
+            height = hei;
+        }
+
+    }
+}
