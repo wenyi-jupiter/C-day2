7daf2c6eafa04e914e3edbdb7ac9e18bb577ec66
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-10 20:26:25 -0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    class Box
+    {
+        double length, breadth, height;
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public void setBreadth(double bre)
+        {
+            breadth = bre;
+        }
+
+        public void setHeight(double hei)
+        {
+            height = hei;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,29 +4,27 @@ namespace Exercise5
 {
     class Program
     {
-       
-
         static void Main(string[] args)
         {
             Box box1 = new Box();
-            box1.setLength(1.1);
-            box1.setWidth(2.3);
-            box1.setHeight(3.2);
-
             Box box2 = new Box();
-            box2.setLength(4);
-            box2.setWidth(5);
-            box2.setHeight(6);
-
             Box box3 = new Box();
+
+            box1.setLength(1);
+            box1.setHeight(2);
+            box1.setBreadth(3);
+
+            box2.setLength(4);
+            box2.setHeight(5);
+            box2.setBreadth(6);
+
             box3.setLength(7);
-            box3.setWidth(8);
-            box3.setHeight(9);
+            box3.setHeight(8);
+            box3.setBreadth(9);
+
             Console.WriteLine($"Volume of Box1 : {box1.getVolume()}");
             Console.WriteLine($"Volume of Box2 : {box2.getVolume()}");
             Console.WriteLine($"Volume of Box3 : {box3.getVolume()}");
-
-
         }
     }
 }
