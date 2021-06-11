8f6d54260cd54c2836da35a8639216ab1080485f
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-10 20:44:01 -0700

    Add files via upload

---
+++ Exercise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public void GetData()
+        {
+            Console.WriteLine("enter length:");
+            Length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter width:");
+            Width = Convert.ToDouble(Console.ReadLine());
+
+        }
+        public double GetArea()
+        {
+            return Length * Width;
+        }
+        public double GetPerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.GetData();
+            Console.WriteLine($"Length: {r.Length}");
+            Console.WriteLine($"Width: {r.Width}");
+            Console.WriteLine($"Area: {r.GetArea()}");
+            Console.WriteLine($"Perimeter: {r.GetPerimeter()}");
+
+        }
+    }
+}
